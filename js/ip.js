fc56ae9971304016a09f2fbc4aafae9eaa7b3d33
wik2kassa <wik2kassa@gmail.com>
2015-10-21 09:45:13 +0530

    Log Filters Added

--- js/ip.js
+++ js/ip.js
@@ -8,7 +8,7 @@ IPIDE.IDE = {
     workSpaceCount: -1,
     doneFirst: false,
     init: function () {
-        $(document).on("click", "#ide_newworkspace", function(event) {
+        $(document).on("click", "#ide_newworkspace", function (event) {
             event.preventDefault();
             IPIDE.IDE.newWorkSpace();
         });
@@ -55,15 +55,18 @@ IPIDE.IDE = {
             event.preventDefault();
             var wid = $(event.target).attr("data-ipide-id");
             var fid = $(event.target).attr("data-ipide-fid");
-            var filteruiid = "#filtercp_" + IPIDE.IDE.workspaces[wid].filters[fid].uiid;
-            $(filteruiid).remove();
-            IPIDE.IDE.workspaces[wid].filters.splice(fid, 1);
+            IPIDE.IDE.workspaces[wid].removeFilter(fid);
 
         });
         $(document).on("click", "a.histogram", function (event) {
             event.preventDefault();
             var id = $(event.target).attr("data-ipide-id");
             IPIDE.IDE.workspaces[id].generateHistogram();
+        });
+        $(document).on("click", "a.exif", function (event) {
+            event.preventDefault();
+            var id = $(event.target).attr("data-ipide-id");
+            IPIDE.IDE.workspaces[id].showMetada();
         });
     },
     newWorkSpace: function () {
@@ -116,6 +119,7 @@ IPIDE.WorkSpace.prototype = {
             "<a class='clone btn btn-danger ip_btn' id='clone_" + input_id + "' data-ipide-id='" + this.id + "'>Clone</a>" +
             "<a class='revert btn btn-warning ip_btn' id='revert_" + input_id + "' data-ipide-id='" + this.id + "'>Revert</a>" +
             "<a class='histogram btn btn-warning ip_btn' id='revert_" + input_id + "' data-ipide-id='" + this.id + "'>Histogram</a>" +
+            "<a class='exif btn btn-warning ip_btn' id='revert_" + input_id + "' data-ipide-id='" + this.id + "'>View EXIF</a>" +
             "</form>" +
             "</div>" +
             "<div class='clearfix'></div>" +
@@ -163,7 +167,8 @@ IPIDE.WorkSpace.prototype = {
         this.canvas.getContext("2d").clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.canvas.getContext("2d").drawImage(this.image, 0, 0);
         for (var filter of this.filters) {
-            filter.apply(this.id);
+            if (filter)
+                filter.apply(this.id);
         }
         if (this.img == null)
             this.img = document.getElementById('img_' + this.id);
@@ -197,15 +202,40 @@ IPIDE.WorkSpace.prototype = {
     generateHistogram: function () {
         this.imageData = this.canvas.getContext('2d').getImageData(0, 0, this.canvas.width, this.canvas.height);
         var data = this.imageData.data;
+        this.histogram = Object.create(IPIDE.Histogram);
         for (var i = 0; i < data.length; i += 4) {
             this.histogram.putR(data[i]);
             this.histogram.putG(data[i + 1]);
             this.histogram.putB(data[i + 2]);
         }
-        this.histogram.getHistogramSrc();
+
+        var histsrc = this.histogram.getHistogramSrc();
+        $("#ipide-modal-content").html("<img src='" + histsrc + "'>").after(function () {
+            $("#ipide-modal-heading").html("Histogram").after(function () {
+                $("#largeModal").modal('show');
+            });
+        });
     },
-    removeFilter(filterid) {
-        this.filters.splice(filterid, 1);
+    removeFilter: function (filterid) {
+        var filteruiid = "#filtercp_" + IPIDE.IDE.workspaces[this.id].filters[filterid].uiid;
+        $(filteruiid).remove();
+        this.filters.splice(filterid, 1, null); //hack :/ added null to not to mess up the index references of following filters
+    },
+    showMetada: function () {
+        EXIF.getData(this.image, function () {
+            var content = EXIF.pretty(this);
+            var myfilename = this.fileName;
+            content = content.replace(/(?:\r\n|\r|\n)/g, '<br />');
+            if (content.length < 2) {
+                content = "No exif data found!";
+            }
+            console.log(content);
+            $("#ipide-modal-content").html(content).after(function () {
+                $("#ipide-modal-heading").html("EXIF data of " + myfilename).after(function () {
+                    $("#largeModal").modal('show');
+                });
+            });
+        });
     }
 
 
@@ -270,7 +300,7 @@ IPIDE.Histogram = {
             if (this.gs[i]) {
                 barheight = (this.gs[i] / this.gsmax ) * 90;
                 ctx.fillRect(i * barlen, barheight, barlen, 100 - barheight);
-                console.log(barheight);
+                //console.log(barheight);
             }
         }
         console.log(this.canvas.toDataURL());
@@ -362,8 +392,7 @@ IPIDE.IDE.registerFilter(IPIDE.NegativeFilter);
 
 IPIDE.BrightnessFilter = Object.create(IPIDE.Filter);
 IPIDE.BrightnessFilter.name = "Brightness Filter";
-IPIDE.BrightnessFilter.apply = function()
-{
+IPIDE.BrightnessFilter.apply = function () {
     this.imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
     var data = this.imageData.data;
     var bump = parseInt($("#brightness_" + this.uiid).val());
@@ -375,7 +404,7 @@ IPIDE.BrightnessFilter.apply = function()
     }
     this.ctx.putImageData(this.imageData, 0, 0);
 }
-IPIDE.BrightnessFilter.getControlPanel = function() {
+IPIDE.BrightnessFilter.getControlPanel = function () {
     var str = "<div class='filtercontrolpanel' id='filtercp_" + this.uiid + "'>" +
         this.getCloseButton() +
         "<h4>" + this.getName() +
@@ -387,8 +416,8 @@ IPIDE.IDE.registerFilter(IPIDE.BrightnessFilter);
 
 IPIDE.ContrastFilter = Object.create(IPIDE.Filter);
 IPIDE.ContrastFilter.name = "Contrast Filter";
-IPIDE.ContrastFilter.apply = function() {
-    this.imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+IPIDE.ContrastFilter.apply = function () {
+    this.imageData = this.canvas.getContext("2d").getImageData(0, 0, this.canvas.width, this.canvas.height);
     var data = this.imageData.data;
     var factor = parseFloat($("#contrast_" + this.uiid).val());
     console.log("Contrast factor " + factor + " " + this.uiid);
@@ -399,7 +428,7 @@ IPIDE.ContrastFilter.apply = function() {
     }
     this.ctx.putImageData(this.imageData, 0, 0);
 };
-IPIDE.ContrastFilter.getControlPanel = function() {
+IPIDE.ContrastFilter.getControlPanel = function () {
     var str = "<div class='filtercontrolpanel' id='filtercp_" + this.uiid + "'>" +
         this.getCloseButton() +
         "<h4>" + this.getName() +
@@ -407,3 +436,205 @@ IPIDE.ContrastFilter.getControlPanel = function() {
     return str;
 };
 IPIDE.IDE.registerFilter(IPIDE.ContrastFilter);
+
+IPIDE.ResizeFilter = Object.create(IPIDE.Filter);
+IPIDE.ResizeFilter.name = "Resize";
+
+IPIDE.ResizeFilter.apply = function () {
+    var mycanvas = this.canvas;
+    var myctx = mycanvas.getContext("2d");
+    var imgurl = mycanvas.toDataURL();
+    var image = new Image();
+
+    var new_width = 100;
+    var new_height = 100;
+
+    image.onload = function () {
+        mycanvas.width = new_width;
+        mycanvas.height = new_height;
+        mycanvas.getContext("2d").drawImage(image, 0, 0, new_width, new_height);
+        console.log("resized " + mycanvas.width);
+        console.log(mycanvas.toDataURL());
+
+    }
+    image.src = imgurl;
+};
+
+IPIDE.ResizeFilter.getControlPanel = function () {
+    var str = "<div class='filtercontrolpanel' id='filtercp_" + this.uiid + "'>" +
+        this.getCloseButton() +
+        "<h4>" + this.getName() +
+        "</h4></div>";
+    return str;
+};
+
+IPIDE.IDE.registerFilter(IPIDE.ResizeFilter);
+
+IPIDE.LOGFilter5 = Object.create(IPIDE.Filter);
+IPIDE.LOGFilter5.name = "5x5 LOG Filter";
+IPIDE.LOGFilter5.apply = function () {
+    this.imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    var data = this.imageData.data;
+    var cwidth = this.canvas.width;
+    var cheight = this.canvas.height;
+    var temp, convolv;
+
+    console.log("5x5 LOG Filter");
+    var kernel =
+            [
+                0, 0, -1, 0, 0,
+                0, -1, -2, -1, 0,
+                -1, -2, 16, -2, -1,
+                0, -1, -2, -1, 0,
+                0, 0, -1, 0, 0
+            ]
+        ;
+    for (var i = 0; i < cheight; i++) {
+        for (var j = 0; j < cwidth; j++) {
+            //grayscale the image
+            data[4 * (j + cwidth * i)] = data[4 * (j + cwidth * i) + 1] = data[4 * (j + cwidth * i) + 2] = Math.round(
+                (data[4 * (j + cwidth * i)] * 0.2989) + //r
+                (data[4 * (j + cwidth * i) + 1] * 0.5870) + //g
+                (data[4 * (j + cwidth * i) + 2] * 0.1140)   //b
+            );
+        }
+    }
+    var dataCopy = new Uint8ClampedArray(data);
+    for (var i = 5; i < cheight - 5; i++) {
+        for (var j = 5; j < cwidth - 5; j++) {
+            temp = 0;
+            for (var k = -2; k <= 2; k++) {
+                for (var l = -2; l <= 2; l++) {
+                    temp += Math.round(((data[4 * (((j + l) + cwidth * (i + k)))]) * kernel[(2 + l) + 5 * (2 + k)]));
+                }
+            }
+            dataCopy[4 * (j + cwidth * i)] = dataCopy[4 * (j + cwidth * i) + 1] = dataCopy[4 * (j + cwidth * i) + 2] = Math.round(temp);
+        }
+    }
+    this.imageData.data.set(dataCopy);
+    this.ctx.putImageData(this.imageData, 0, 0);
+}
+IPIDE.LOGFilter5.getControlPanel = function () {
+    var str = "<div class='filtercontrolpanel' id='filtercp_" + this.uiid + "'>" +
+        this.getCloseButton() +
+        "<h4>" + this.getName() +
+        "</h4></div>";
+    return str;
+}
+
+IPIDE.IDE.registerFilter(IPIDE.LOGFilter5);
+
+
+IPIDE.LOGFilter9 = Object.create(IPIDE.Filter);
+IPIDE.LOGFilter9.name = "7x7 LOG Filter";
+IPIDE.LOGFilter9.apply = function () {
+    this.imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    var data = this.imageData.data;
+    var cwidth = this.canvas.width;
+    var cheight = this.canvas.height;
+    var temp;
+
+    console.log("7x7 LOG Filter");
+    var kernel =
+            [
+                0, 0, 1, 1, 1, 0, 0,
+                0, 1, 3, 3, 3, 1, 0,
+                1, 3, 0, -7, 0, 3, 1,
+                1, 3, -7, -24, -7, 3, 1,
+                1, 3, 0, -7, 0, 3, 1,
+                0, 1, 3, 3, 3, 1, 0,
+                0, 0, 1, 1, 1, 0, 0
+            ]
+        ;
+    for (var i = 0; i < cheight; i++) {
+        for (var j = 0; j < cwidth; j++) {
+            //grayscale the image
+            data[4 * (j + cwidth * i)] = data[4 * (j + cwidth * i) + 1] = data[4 * (j + cwidth * i) + 2] = Math.round(
+                (data[4 * (j + cwidth * i)] * 0.2989) + //r
+                (data[4 * (j + cwidth * i) + 1] * 0.5870) + //g
+                (data[4 * (j + cwidth * i) + 2] * 0.1140)   //b
+            );
+        }
+    }
+    var dataCopy = new Uint8ClampedArray(data);
+    for (var i = 7; i < cheight - 7; i++) {
+        for (var j = 7; j < cwidth - 7; j++) {
+            temp = 0;
+            for (var k = -3; k <= 3; k++) {
+                for (var l = -3; l <= 3; l++) {
+                    temp += Math.round(((data[4 * (((j + l) + cwidth * (i + k)))]) * kernel[(3 + l) + 7 * (3 + k)]));
+                }
+            }
+            dataCopy[4 * (j + cwidth * i)] = dataCopy[4 * (j + cwidth * i) + 1] = dataCopy[4 * (j + cwidth * i) + 2] = temp;
+        }
+    }
+    this.imageData.data.set(dataCopy);
+    this.ctx.putImageData(this.imageData, 0, 0);
+}
+IPIDE.LOGFilter9.getControlPanel = function () {
+    var str = "<div class='filtercontrolpanel' id='filtercp_" + this.uiid + "'>" +
+        this.getCloseButton() +
+        "<h4>" + this.getName() +
+        "</h4></div>";
+    return str;
+}
+
+IPIDE.IDE.registerFilter(IPIDE.LOGFilter9);
+
+
+IPIDE.CustomKernelFilter = Object.create(IPIDE.Filter);
+IPIDE.CustomKernelFilter.name = "3x3 Custom Filter";
+IPIDE.CustomKernelFilter.apply = function () {
+    this.imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    var data = this.imageData.data;
+    var cwidth = this.canvas.width;
+    var cheight = this.canvas.height;
+    var temp;
+
+    console.log("3x3 Custom Filter");
+    var inputtext = $("#ckfilter_" + this.uiid).val().split(",");
+    var kernel =
+            [
+                0,0,0,
+                0,0,0,
+                0,0,0
+            ]
+        ;
+    for (var i  = 0;i < inputtext.length;i++) {
+        kernel[i] = parseInt(inputtext[i]);
+    }
+
+    for (var i = 0; i < cheight; i++) {
+        for (var j = 0; j < cwidth; j++) {
+            //grayscale the image
+            data[4 * (j + cwidth * i)] = data[4 * (j + cwidth * i) + 1] = data[4 * (j + cwidth * i) + 2] = Math.round(
+                (data[4 * (j + cwidth * i)] * 0.2989) + //r
+                (data[4 * (j + cwidth * i) + 1] * 0.5870) + //g
+                (data[4 * (j + cwidth * i) + 2] * 0.1140)   //b
+            );
+        }
+    }
+    var dataCopy = new Uint8ClampedArray(data);
+    for (var i = 3; i < cheight - 3; i++) {
+        for (var j = 3; j < cwidth - 3; j++) {
+            temp = 0;
+            for (var k = -1; k <= 1; k++) {
+                for (var l = -1; l <= 1; l++) {
+                    temp += Math.round(((data[4 * (((j + l) + cwidth * (i + k)))]) * kernel[(1 + l) + 3 * (1 + k)]));
+                }
+            }
+            dataCopy[4 * (j + cwidth * i)] = dataCopy[4 * (j + cwidth * i) + 1] = dataCopy[4 * (j + cwidth * i) + 2] = temp;
+        }
+    }
+    this.imageData.data.set(dataCopy);
+    this.ctx.putImageData(this.imageData, 0, 0);
+}
+IPIDE.CustomKernelFilter.getControlPanel = function () {
+    var str = "<div class='filtercontrolpanel' id='filtercp_" + this.uiid + "'>" +
+        this.getCloseButton() +
+        "<h4>" + this.getName() +
+        "</h4><input type='text' id='ckfilter_"+this.uiid+"' placeholder='1,2,3,4,5,6,7,8,9'></div>";
+    return str;
+}
+
+IPIDE.IDE.registerFilter(IPIDE.CustomKernelFilter);
